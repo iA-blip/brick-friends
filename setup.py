3261deedbbc7509ca30ca1aed944e692eeb4a5c5
iA-blip <62136793+iA-blip@users.noreply.github.com>
2022-03-04 16:06:01 +0100

    Fixed some package metadata

--- setup.py
+++ setup.py
@@ -4,15 +4,30 @@ with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name="brick-friends",
-    version="1.0",
-    description="Command-line tool to friend people on brick-hill.",
-    author="iA-blip",
-    license=license,
-    packages=find_packages(exclude=("docs")),
-    entry_points={
+    name = "brick-friends",
+    packages = ["brick-friends"],
+    version = "1.0",
+    description = "Command-line tool to friend people on brick-hill.",
+    author = "iA-blip",
+    url = "https://github.com/iA-blip/brick-friends",
+    license = license,
+    packages = find_packages(),
+    keywords = ["brick-hill", "cli"],
+    entry_points = {
         "console_scripts": [
             "brick-friends=src.cli:main"
         ],
-    }
+    },
+    install_requires = [
+        "argparse",
+        "requests",
+        "pypac",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Installation/Setup",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
 )
